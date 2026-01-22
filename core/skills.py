eb3a47d6d58b086ea133d405c32f976fd3ac7113
ItsTrudeauver <smz.osaragi@gmail.com>
2026-01-22 23:44:35 +0700

    added boss battle for TwT

--- core/skills.py
+++ core/skills.py
@@ -5,6 +5,14 @@
 # core/skills.py
 
 SKILL_DATA = {
+    "Queen of the Zodiacs": {
+        "description": "Trigger 1 random effect at the start of the battle: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Sheep, Monkey, Rooster, Dog, Pig. \nRat: Increase self-power by 10%.\n Ox: Decrease one opponent character's power by 15%. \n Tiger: Increase team power by 5%. \n Rabbit: Decrease opponent team power by 7%. \n Dragon: This character defaults to highest possible variance point (1.1). \n Snake: When losing battle, draw instead. \n Horse: Decrease this character's power by 10%. Increase a random teammate's power by 20%. \n Sheep: Turn opponent's highest power character's power to this unit's power for this battle. \n Monkey: Swap this character's power with a random opponent's character for this battle. \n Rooster: Increase all allies' power by 3% and this character's power by 6%. \n Dog: Copy your strongest character's power to this character for this battle. \n Pig: Disable one random skill from opponent team for this battle.",
+        "value": [0.1, 0.15, 0.05, 0.07, 1.1, 0, [0.1, 0.2], [0.03, 0.06]], # Representing the 777% multiplier
+        "applies_in": "b",
+        "stackable": False,
+        "overlap": False
+    },
+    
     "Lucky 7": {
         "description": "7% chance of +777% power; 77% chance of +7,777 flat power.",
         "value": 7.77, # Representing the 777% multiplier
